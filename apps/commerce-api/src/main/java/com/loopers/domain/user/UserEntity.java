6f5c53ab7d4f7a4fc215520c7b7c6796191c2057
taehwan0 <jang_taehwan@naver.com>
2025-07-15 12:48:25 +0900

    feat: UserEntity 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserEntity.java
@@ -0,0 +1,57 @@
+package com.loopers.domain.user;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Pattern;
+
+@Table(name = "member")
+@Entity
+public class UserEntity extends BaseEntity {
+
+	private static final Pattern USER_ID_PATTERN = Pattern.compile("^[a-zA-Z0-9]{1,10}$");
+	// email pattern은 약식으로 사용한다.
+	// xx@yy.zz 수준이면 성공시킨다.
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+	private static final Pattern BIRTH_PATTERN = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+
+	private String userId;
+	private String name;
+	private Gender gender;
+	private LocalDate birth;
+	private String email;
+
+	protected UserEntity() {
+		super();
+	}
+
+	public UserEntity(String userId, String name, Gender gender, String birth, String email) {
+		if (userId == null || !USER_ID_PATTERN.matcher(userId).matches()) {
+			throw new CoreException(ErrorType.BAD_REQUEST, "userId는 영문/숫자 10자 이내로만 가능합니다.");
+		}
+
+		if (email == null || !EMAIL_PATTERN.matcher(email).matches()) {
+			throw new CoreException(ErrorType.BAD_REQUEST, "email 형식이 잘못되었습니다.");
+		}
+
+		if (birth == null || !BIRTH_PATTERN.matcher(birth).matches()) {
+			throw new CoreException(ErrorType.BAD_REQUEST, "생년월일 형식이 잘못되었습니다.");
+		}
+
+		try {
+			LocalDate.parse(birth);
+		} catch (DateTimeParseException e) {
+			throw new CoreException(ErrorType.BAD_REQUEST, "생년월일 형식이 잘못되었습니다.");
+		}
+
+		this.userId = userId;
+		this.name = name;
+		this.gender = gender;
+		this.birth = LocalDate.parse(birth);
+		this.email = email;
+	}
+}
