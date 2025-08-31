4125147341ae84323701224ae72839f36e531fd5
taehwan0 <jang_taehwan@naver.com>
2025-08-31 18:28:31 +0900

    fix: CouponListener event comsumer 수정
    - TransactionalEventListener -> EventListener로 수정
      - publish 시 Transactional을 사용하지 않아, 정상적으로 consume되지 않을 것으로 확인됨

--- apps/commerce-api/src/main/java/com/loopers/interfaces/listener/coupon/CouponListener.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/listener/coupon/CouponListener.java
@@ -3,10 +3,8 @@ package com.loopers.interfaces.listener.coupon;
 import com.loopers.application.coupon.CouponEventHandler;
 import com.loopers.domain.payment.PaymentEvent;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
@@ -14,8 +12,7 @@ public class CouponListener {
 
 	private final CouponEventHandler couponEventHandler;
 
-	@Async
-	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@EventListener
 	public void handlePaymentSuccessEvent(PaymentEvent.PaymentSuccess event) {
 		couponEventHandler.useCoupon(event);
 	}
